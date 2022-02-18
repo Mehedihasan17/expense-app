9a8a19b7d468ed19909c777c880facc3db3e9fbe
Mehdedihasan17 <mahadikhan661@gmail.com>
2022-02-18 23:35:00 +0600

    all

---
+++ js/app.js
@@ -0,0 +1,149 @@
+
+function getError() {
+    const err = document.getElementById('err-msg');
+    return err;
+}
+function getErrorExpenses() {
+    const err = document.getElementById('err-msg2');
+    return err;
+}
+//error messages
+function getEmptyError() {
+    const err = document.getElementById('err-msg-empty');
+    return err;
+}
+function getAmountError() {
+    const err = document.getElementById('err-msg-amount');
+    return err;
+}
+
+function getExpenses() {
+    //expenses......
+    const foodInput = document.getElementById('food-input');
+    const foodCost = parseFloat(foodInput.value);
+    const rentInput = document.getElementById('rent-input');
+    const rentCost = parseFloat(rentInput.value);
+    const clothesInput = document.getElementById('clothes-input');
+    const clothesCost = parseFloat(clothesInput.value);
+
+    function clearfield() {
+        foodInput.value = '';
+        rentInput.value = '';
+        clothesInput.value = '';
+    }
+    //error
+    if (isNaN(foodCost) || isNaN(rentCost) || isNaN(clothesCost) || foodCost < 0 || rentCost < 0 || clothesCost < 0) {
+        const error = getError();
+        error.style.display = 'block';
+        const error2 = getErrorExpenses();
+        error2.style.display = 'none';
+    }
+    else {
+        // total expenses
+        const totalExpenses = foodCost + rentCost + clothesCost;
+        const expensesField = document.getElementById('total-expenses');
+        expensesField.innerText = totalExpenses;
+        const error = getError();
+        error.style.display = 'none';
+        const error2 = getErrorExpenses();
+        error2.style.display = 'none';
+        return totalExpenses;
+    }
+
+}
+function getIncome() {
+    const incomeInput = document.getElementById('income-input');
+    const income = parseFloat(incomeInput.value);
+    //error
+    if (isNaN(income) || income < 0) {
+        const error = getError();
+        error.style.display = 'block';
+        const error2 = getErrorExpenses();
+        error2.style.display = 'none';
+    }
+    else {
+        return income;
+    }
+}
+function getBalance() {
+    const totalIncome = getIncome();
+    const totalExpenses = getExpenses();
+    //calculating balance
+    if (isNaN(totalIncome) || isNaN(totalExpenses)) {
+        const error = getError();
+        error.style.display = 'block';
+        const error2 = getErrorExpenses();
+        error2.style.display = 'none';
+    }
+    else {
+        if (totalExpenses > totalIncome) {
+            const error = getError();
+            error.style.display = 'none';
+            const error2 = getErrorExpenses();
+            error2.style.display = 'block';
+            const expensesField = document.getElementById('total-expenses');
+            expensesField.innerText = '';
+
+
+        }
+        else {
+            const balance = totalIncome - totalExpenses;
+            const balanceField = document.getElementById('balance');
+            balanceField.innerText = balance;
+            // error msg
+            const error = getError();
+            error.style.display = 'none';
+            const error2 = getErrorExpenses();
+            error2.style.display = 'none';
+            return balance;
+        }
+    }
+}
+function getRemainingBalance() {
+    //After balance
+    const saveInput = document.getElementById('save-input');
+    const save = parseFloat(saveInput.value);
+    //calculating saving amount
+    if (isNaN(save) || save < 0) {
+        const error = getEmptyError();
+        error.style.display = 'block';
+
+        const savingField = document.getElementById('saving-amount');
+        savingField.innerText = '';
+        const remainBalanceField = document.getElementById('remaining-balance');
+        remainBalanceField.innerText = '';
+    }
+    else {
+        const saveIncome = getIncome();
+        const savingAmount = (saveIncome * save) / 100;
+        const totalBalance = getBalance()
+        //calculating remaining balance
+        if (savingAmount > totalBalance) {
+            const error = getAmountError();
+            error.style.display = 'block';
+            const savingField = document.getElementById('saving-amount');
+            savingField.innerText = '';
+            const remainBalanceField = document.getElementById('remaining-balance');
+            remainBalanceField.innerText = '';
+        }
+        else {
+            const remainingBalance = totalBalance - savingAmount;
+
+            const savingField = document.getElementById('saving-amount');
+            savingField.innerText = savingAmount;
+            const remainBalanceField = document.getElementById('remaining-balance');
+            remainBalanceField.innerText = remainingBalance;
+            const error = getAmountError();
+            error.style.display = 'none';
+        }
+    }
+}
+
+//Calculate button
+document.getElementById('button-calculate').addEventListener('click', function () {
+    const balance = getBalance();
+})
+//Save Button
+document.getElementById('save-btn').addEventListener('click', function () {
+    const remainingBalance = getRemainingBalance();
+})
